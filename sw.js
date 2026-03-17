02fd74b2590a104956556d3b9a50bebc966cffd1
Ameen E <ayishaameen491@gmail.com>
2026-03-17 06:34:26 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,25 @@
-self.addEventListener("install", e => {
-  console.log("Service Worker Installed");
+const CACHE_NAME = "ameen-site-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/profile-img.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener("fetch", e => {});
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
